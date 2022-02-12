5bcd39dcaea39886b15c98d6594ddb7012e447cf
camilleNicole <camilleht@gmail.com>
2022-02-11 19:03:09 -0500

    app.js fix

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+
 const generatePage = require('./projects/portfolio-generator/src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
+console.log(name, github);
 
-
+const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
